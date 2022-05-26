ab25850f6f637323be8c77b9af28a7a4a26c87e0
athiratp007 <athiratp007@example.com>
2022-05-26 11:35:46 +0530

    add ui

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Addstud from './Components/Addstud';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' exact element={<Addstud/>}/>
+      </Routes>
+      </BrowserRouter>
 
     </div>
     
--- src/Components/Addstud.js
+++ src/Components/Addstud.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import Nave from './Nave'
 
 const Addstud = () => {
   return (
-    <div>Addstud</div>
+    <div>
+        <Nave/>
+<div class="container">
+    <div class="row">
+        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div class="row g-3">
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label"> Name</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                   <label for="" class="form-label">AdmissionNo</label>
+                   <input type="text" class="form-control"/>
+                   
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">CGPA</label>
+                    <input type="number" name="" id="" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <button class="btn btn-success">SUBMIT</button> 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </div>
   )
 }
 
