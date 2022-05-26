21c2e7d2a9b740868ff8f6f49e688c8d25836433
athiratp007 <athiratp007@example.com>
2022-05-26 12:30:32 +0530

    search andview

--- src/Components/Nave.js
+++ src/Components/Nave.js
@@ -5,17 +5,18 @@ const Nave = () => {
   return (
     <div>
 
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <Link class="navbar-brand" to="/">MZC</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg navbar-light bg-success ">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/">MZC</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
     
-        <Link  class="nav-link" to="/">Addstud</Link>
-        <Link  class="nav-link" to="/search">Searchstud</Link>
+        <Link  className="nav-link" to="/">Addstud</Link>
+        <Link  className="nav-link" to="/search">Searchstud</Link>
+        <Link  className="nav-link" to="/view">View</Link>
     
         
       </div>
